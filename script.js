861be96f1405b22aef84e0ac03227489daf3b28f
sha0002 <kshashank960@gmail.com>
2024-12-01 11:47:04 +0530

    Background change

---
+++ script.js
@@ -0,0 +1,19 @@
+// let color = document.querySelector("button").onclick = function(){
+//     document.body.style.background = ("red")
+
+// }
+
+// function clickbutton(color) {
+//     return function () {
+//         document.body.style.background = `${color}`
+//         console.log("click the color")
+//     }
+// }
+
+// document.querySelector("button").onclick = clickbutton("red")
+
+function clickbutton() {
+    document.body.style.background = ("red")
+    document.querySelector("h1").onclick = document.body.style.color = ("white")
+}
+
